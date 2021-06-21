b89e8527a6bc22ab8a2d5a0ca3cb0a5e2d7bd7db
Osc <reosc456@gmail.com>
2021-06-21 14:45:12 -0600

    Arreglo de Jugador

--- R2/Jugador/Jugador.java
+++ R2/Jugador/Jugador.java
@@ -1,6 +1,7 @@
 package R2.Jugador;
 import R2.Principal.*;
 public class Jugador {
+    private int id;
     private int partidasGanadas = 0;
     private int partidasPerdidas = 0;
     private int puntuacionTotal = 0;
@@ -8,7 +9,8 @@ public class Jugador {
     private IngresarDatos leerDatos = new IngresarDatos();
     private int contadorJugadores = 0;
 
-    public Jugador(int partidasGanadas, String nombreJugador, int partidasPerdidas, int puntuacionTotal){
+    public Jugador(int id, int partidasGanadas, String nombreJugador, int partidasPerdidas, int puntuacionTotal){
+        this.id = id;
         this.partidasGanadas = partidasGanadas;
         this.nombreJugador = nombreJugador;
         this.partidasPerdidas = partidasPerdidas;
@@ -35,10 +37,16 @@ public class Jugador {
     public void agregarJugador(Jugador[] listado){
         String nombreJugador = "";
         nombreJugador = leerDatos.ingresarString("Ingrese el nombre del Jugador");
-        listado[contadorJugadores] =  new Jugador(0, nombreJugador, 0, 0);
+        listado[contadorJugadores] =  new Jugador(0,0, nombreJugador, 0, 0);
         contadorJugadores++;
     }
     ////////////////////////////////
+    public int getid(){
+        return id;
+    }
+    public void setid(int id){
+         this.id = id;
+    }
     public int getPartidasGanadas(){
         return partidasGanadas;
     }
