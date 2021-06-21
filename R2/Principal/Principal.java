937515ab5fbf6e45119acbceae6effd3441ab83a
Osc <reosc456@gmail.com>
2021-06-21 15:05:05 -0600

    Adicion duda

--- R2/Principal/Principal.java
+++ R2/Principal/Principal.java
@@ -1,8 +1,15 @@
 package R2.Principal;
+import R2.Jugador.*;
+import R2.Tablero.*;
 import java.util.*;
+import R2.Manejadores.*;
 public class Principal{
     Scanner sn = new Scanner(System.in);
     Scanner sc = new Scanner(System.in);
+    Tablero r = new Tablero();
+    private Jugador Player = new Jugador();
+    private Jugador [] Players = new Jugador[10];
+    private ManejadorInicio M = new ManejadorInicio(Players);
     public static void main(String[] args) {
         Principal e = new Principal();
         e.Menu();
@@ -24,16 +31,16 @@ public class Principal{
                  opcion = sn.nextInt();
                  switch (opcion) {
                      case 1:
-                        
+                          M.IniciarJuego();
                          break;
                      case 2:
-                          
+                         Player.agregarJugador(Players); 
                          break;
                      case 3:
-                         
+                          Player.listadoJugadores(Players);
                          break;
                      case 4:
-                       
+                          r.ImprimirTablero();
                          break;
                      case 5:
                          salir = true;
@@ -48,4 +55,5 @@ public class Principal{
              }
          }
     }
+ ///////////////////
 }
